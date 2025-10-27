7e2d2f45d016d5a09570d9b7061ce2ff0a3cdb3d
Anushka Singh <144716175+A-N-U-S-I-N@users.noreply.github.com>
2025-10-27 20:09:35 +0530

    Fix string interpolation in axios post request

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -18,7 +18,7 @@ export default function Login({setUser}) {
   const submit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("${process.env.REACT_APP_API_URL}/api/users/login", form);
+      const res = await axios.post(`${process.env.REACT_APP_API_URL}/api/users/login`, form);
       localStorage.setItem("token", res.data.token);
       setUser(res.data.user); 
       alert("Login successful!");
