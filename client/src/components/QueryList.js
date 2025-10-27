b89f80080bdc4213ad092794b235684c1fd8521c
Anushka Singh <144716175+A-N-U-S-I-N@users.noreply.github.com>
2025-10-27 19:59:50 +0530

    Update API URL to use environment variable

--- client/src/components/QueryList.js
+++ client/src/components/QueryList.js
@@ -6,7 +6,7 @@ const QueryList = () => {
   const [queries, setQueries] = useState([]);
 
   useEffect(() => {
-    axios.get('http://localhost:5000/api/queries')
+    axios.get('${process.env.REACT_APP_API_URL}/api/queries')
       .then(res => setQueries(res.data))
       .catch(() => setQueries([]));
   }, []);
